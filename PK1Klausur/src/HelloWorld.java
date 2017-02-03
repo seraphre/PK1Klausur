dc16d10fd10798804d31fa88ddabb00565357089
Peking <Peking@Peking-PC.Speedport_W_724V_01011603_00_008>
2017-02-03 12:47:02 +0100

    Noch ein weiterer Test

--- PK1Klausur/src/HelloWorld.java
+++ PK1Klausur/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Zweiter Test");
+		System.out.println("Noch ein Test!");
 
 	}
 
