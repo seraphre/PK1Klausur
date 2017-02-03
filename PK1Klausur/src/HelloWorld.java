d402dddcfbcbb1a0c65db781185c4f44e9bae535
thanhnguyen <thanhnguyen@192.168.2.104>
2017-02-03 13:25:01 +0100

    Noch ein Test vom Mac

--- PK1Klausur/src/HelloWorld.java
+++ PK1Klausur/src/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Zweiter Test");
 		System.out.println("Noch ein Test!");
+		
+		System.out.println("Gepushed from Mac");
+		System.out.println("Das gleich nochmal");
 
 	}
 
