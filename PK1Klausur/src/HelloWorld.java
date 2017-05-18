8dba3e0ddc0d24e8676474cbb2721fea6eda790b
seraphre <seraph.nguyen@gmail.com>
2017-05-18 23:09:49 +0200

    Nächster Test halt

--- PK1Klausur/src/HelloWorld.java
+++ PK1Klausur/src/HelloWorld.java
@@ -3,16 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Zweiter Test");
-		System.out.println("Noch ein Test!");
-		
-		System.out.println("Gepushed from Mac");
-		System.out.println("Das gleich nochmal");
-		System.out.println("Das gleiche nochmal");
-		
-		System.out.println("Das noch ein TEst nochmal");
-		
-		System.out.println("Das noch ein letzer TEst nochmal");
+		System.out.println("nächster Test");
+		System.out.println("zweiter Test");
 
 
 	}
